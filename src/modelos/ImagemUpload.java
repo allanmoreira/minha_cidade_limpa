66fd04bf17da8e1ca9f826fad1efb3eec73c5883
ViniciusAz <viniciussazevedo@gmail.com>
2015-10-23 06:52:34 -0200

    Upload de Imagem melhorias
    Cria diretorio automaticamente baseado no caminho do projeto, e
    prototipo suporte v3.0

--- src/modelos/ImagemUpload.java
+++ src/modelos/ImagemUpload.java
@@ -1,8 +1,12 @@
 package modelos;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -10,57 +14,50 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.tomcat.util.http.fileupload.FileItemFactory;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
 
 //@WebServlet("/ImagemUpload")
 public class ImagemUpload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	// teste - location to store file uploaded
-    private static final String UPLOAD_DIRECTORY = "upload";
+	// caminho da pasta de imagens:
+    private static final String DIRETORIO = System.getProperty("user.dir") + "/upload_imagens/";
+    // outra opcao : getServletContext().getRealPath("") + File.separator + pasta?!?;
  
-    // Se quiser limitar o tamanho da imagem:
-    private static final int TAMANHO_MAX   = 1024 * 1024 * 3;  // 3MB
-    
+    // Se quiser limitar o tamanho da imagem: - testar
+    private static final int TAMANHO_MAX   = 1024 * 1024 * 10;  // 10MB
     
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+		// verifica se e multipart (parametro do form)
 		if (!ServletFileUpload.isMultipartContent(request)) {
-            // if not, we stop here
+			
             PrintWriter writer = response.getWriter();
-            writer.println("Error: Form must has enctype=multipart/form-data.");
-            writer.flush();
+            //writer.println("Error: Form must has enctype=multipart/form-data.");
+            //writer.flush();
             return;
         }
 	    
 		// Create a factory for disk-based file items
 		DiskFileItemFactory factory = new DiskFileItemFactory();
 
-		// Configure a repository
-		factory.setRepository(new File("\\temp"));
+		//factory.setRepository(new File("\\temp"));
         //? factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
-
+		
 		ServletFileUpload upload = new ServletFileUpload(factory);
 
-		// Limitar o tamanho da foto (opcional):
+		//Limitar o tamanho da foto (opcional):
 		//upload.setFileSizeMax(TAMANHO_MAX);
          
-		/****** TESTE novo DIRETORIO SERVER *****/
-        // constructs the directory path to store upload file
-        // this path is relative to application's directory
-        String uploadPath = getServletContext().getRealPath("")
-                + File.separator + UPLOAD_DIRECTORY;
-         
-        // creates the directory if it does not exist
-        File uploadDir = new File(uploadPath);
-        if (!uploadDir.exists()) {
-            uploadDir.mkdir();
-        }
-        /****** TESTE DIRETORIO SERVER fim *****/
-        
+
         try {
             // parses the request's content to extract file data
             @SuppressWarnings("unchecked")
@@ -71,14 +68,22 @@ public class ImagemUpload extends HttpServlet {
                 for (FileItem item : formItems) {
                     
                     if (!item.isFormField()) {
-                        String fileName = new File(item.getName()).getName();
-                        String filePath = uploadPath + File.separator + fileName;
-                        File storeFile = new File(filePath);
- 
+                        //String fileName = new File(item.getName()).getName();
+                        //String filePath = uploadPath + File.separator + fileName;
+                    	
+                        String fileName = item.getName();    
+                        //String root = getServletContext().getRealPath("/");
+                        File path = new File(DIRETORIO);
+                        if (!path.exists()) {
+                        	path.mkdir();
+                        }
+
                         // saves the file on disk
-                        item.write(storeFile);
-                        request.setAttribute("message",
-                            "Upload has been done successfully!");
+                        File uploadedFile = new File(path + fileName);
+                        //System.out.println(uploadedFile.getAbsolutePath());
+                        item.write(uploadedFile);
+                        
+                        //request.setAttribute("message", "Upload has been done successfully!");
                     }
                 }
             }
@@ -88,3 +93,16 @@ public class ImagemUpload extends HttpServlet {
         }
     }
 }
+
+/* SERVLET 3.0 Exemplo - testando
+ * 
+ 	Part file = request.getPart("file"); // Retrieves <input type="file" name="file">
+	InputStream input = file.getInputStream();
+	OutputStream output = new FileOutputStream(new File(System.getProperty("user.dir") + "/pasta_fotos/", file.getName()));
+	try {
+        IOUtils.copy(input, output);
+    } finally {
+        IOUtils.closeQuietly(input);
+        IOUtils.closeQuietly(output);
+    }
+ */
